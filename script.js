1ef036beb3111777007acdd60a83363b7189381a
flyrix68 <kacoujunior68@gmail.com>
2025-03-30 11:17:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,5 @@ document.addEventListener('DOMContentLoaded', function() {
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
   });
 
-  // Gérer la soumission du formulaire
-  document.getElementById('ajouterCoursForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    console.log("Formulaire soumis !"); // Testez dans la console (F12)
-    // Ici, vous pouvez ajouter la logique pour envoyer les données
-  });
+ 
 });

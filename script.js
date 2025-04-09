8960412daccb3fcb98dbf62113d408b5f55d6680
Kacou <kacoujunior98@gmail.com>
2025-04-09 15:59:57 +0000

    update

--- script.js
+++ script.js
@@ -5,9 +5,5 @@ document.addEventListener('DOMContentLoaded', function() {
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
   });
 
-<<<<<<< HEAD
-
-=======
  
->>>>>>> 1ef036beb3111777007acdd60a83363b7189381a
 });
